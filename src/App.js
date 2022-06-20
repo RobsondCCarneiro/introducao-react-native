fc63a8064819dfc03ed7e19f5b24f0ca4841d505
RobsondCCarneiro <robinho.cenep@gmail.com>
2022-06-19 23:54:29 -0300

    Conhecendo o StyleSheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
 const App = () => {
-    return <View>
-        <Text>Hello World</Text>
+    return <View style={style.container}>
+        <Text style={style.text}>Hello World</Text>
     </View>;
 };
 
 export default App;
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: 'pink',
+        flex: 1, //Expandindo a cor para a tela inteira
+    },
+    text: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'black',
+    }
+});
